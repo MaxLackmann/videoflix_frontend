6b089493c01019f332eba420f50f049bbeb573f2
MaxLackmann <lackmann_max@hotmail.com>
2025-04-27 17:31:14 +0200

    added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'videoflix_frontend';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { ForgotPasswordComponent } from './features/auth/forgot-password/forgot-password.component';
+import { VideoPlayerComponent } from './features/video-player/video-player.component';
+import { PrivacyPolicyComponent } from './features/privacy-policy/privacy-policy.component';
+import { ImprintComponent } from './features/imprint/imprint.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DashboardComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'forgot-password', component: ForgotPasswordComponent },
+    { path: 'video/:id', component: VideoPlayerComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: '**', redirectTo: '' }
+];
